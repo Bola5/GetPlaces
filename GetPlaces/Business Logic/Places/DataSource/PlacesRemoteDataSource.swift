17da557508d55de989bc97248938de95e4fecb97
Bola Fayez <bola.fayez@yahoo.com>
2023-01-01 14:59:08 +0200

    Feat: add the remote DataSource

--- GetPlaces/Business Logic/Places/DataSource/PlacesRemoteDataSource.swift
+++ GetPlaces/Business Logic/Places/DataSource/PlacesRemoteDataSource.swift
@@ -9,7 +9,7 @@
 import Foundation
 
 protocol PlacesRemoteDataSourceSourceProtocol {
-    
+    func fetchPlaces(lat: String, lon: String, completion: @escaping (Result<PlacesModel, ErrorManager>) -> Void)
 }
 
 class PlacesRemoteDataSource: PlacesRemoteDataSourceSourceProtocol {
@@ -20,6 +20,15 @@ class PlacesRemoteDataSource: PlacesRemoteDataSourceSourceProtocol {
         self.communicationManagerProtocol = communicationManagerProtocol
     }
 
-    
-    
+    func fetchPlaces(lat: String, lon: String, completion: @escaping (Result<PlacesModel, ErrorManager>) -> Void) {
+        communicationManagerProtocol.request(urlString: EndPoints.fetchPlaces(lat: lat, lon: lon).asRequest(), completion: { (result : Result<PlacesModel, ErrorManager>) in
+            switch result {
+                case .success(let model):
+                completion(.success(model))
+                case .failure(let error):
+                completion(.failure(error))
+            }
+        })
+    }
+
 }

09d0ee367eff0708e7be5ea765c5417bd2a78ad1
Bola Fayez <bola.fayez@yahoo.com>
2023-01-01 15:09:46 +0200

    Feat: setup get the current city from location

--- GetPlaces/Presentation/Places/PlacesViewController.swift
+++ GetPlaces/Presentation/Places/PlacesViewController.swift
@@ -6,15 +6,54 @@
 //
 
 import UIKit
+import CoreLocation
 
 class PlacesViewController: UIViewController {
 
+    // MARK: - Properties
+    private let locationManager = CLLocationManager()
+
+    // MARK: - viewDidLoad
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        setupViews()
+        getCurrentLocation()
+    }
+
+    // MARK: - setupViews
+    private func setupViews() {
         
         view.backgroundColor = .white
     }
 
+}
 
+// MARK: - Get city from current location
+extension PlacesViewController: CLLocationManagerDelegate {
+    
+    // MARK: - getCurrentLocation
+    private func getCurrentLocation() {
+        
+        locationManager.requestWhenInUseAuthorization()
+        
+        DispatchQueue.global().async { [weak self] in
+            if CLLocationManager.locationServicesEnabled() {
+                self?.locationManager.delegate = self
+                self?.locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
+                self?.locationManager.requestAlwaysAuthorization()
+                self?.locationManager.startUpdatingLocation()
+            }
+        }
+
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let locValue: CLLocationCoordinate2D = manager.location?.coordinate else { return }
+        print(locValue.latitude)
+        print(locValue.longitude)
+        
+    }
+    
 }
 
